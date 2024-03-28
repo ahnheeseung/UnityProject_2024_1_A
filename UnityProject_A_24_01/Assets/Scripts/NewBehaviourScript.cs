3037c5d7560e46417abc7dd3503d02430e73f21d
ahnheeseung <01024195082a@gmail.com>
2024-03-28 15:58:16 +0900

    unityproject_A_24_01

--- UnityProject_A_24_01/Assets/Scripts/NewBehaviourScript.cs
+++ UnityProject_A_24_01/Assets/Scripts/NewBehaviourScript.cs
@@ -8,22 +8,30 @@ public class NewBehaviourScript : MonoBehaviour
     public Text TextUI = null;  //텍스트UI
     public int Count = 0;  //마우스 클릭 카운터
     public int Power = 100; // 물리 힘 수치
+    public int Point = 0;
+    public float checkTime = 0.0f;
     public Rigidbody m_Rigidbody;  //오브젝트의 강체
 
     // Update is called once per frame
     void Update()
     {
+        checkTime += Time.deltaTime;
+        if(checkTime >=1.0f )
+        {
+            Point += 1;
+            checkTime = 0.0f;
+        }
            if (Input.GetMouseButtonDown(0)) //마우스 왼쪽 입력이 되었을 때 조건
         {
-            Count += 1; //마우스가 클릭되었을때 Count를 1씩 올린다
-            TextUI.text = Count.ToString(); //UI를 갱신
             Power = Random.Range(100, 200); //100~200 사이의 값의 힘을 준다.
             m_Rigidbody.AddForce(transform.up * Power); //Y축으로 설정한 힘을 준다.
          }
-           if(gameObject.transform.position.y >= 2 || gameObject.transform.position.y <= -2)
-        {  //오브젝트의  y값이 -2이하이거나 2이상일경우 조건문
-            TextUI.text = "실패";
-            Count = 0; //실패시 카운터 초기화
-        }
+        TextUI.text = Point.ToString();
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        // Debug.Log(collision.gameObject.name);
+        Point = 0;
+        gameObject.transform.position = Vector3.zero;
     }
 }
